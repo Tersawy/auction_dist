11f4f9b9c16a95b69167d1f6c8b5149c02556f37
Tersawy <mohamedtersawy96@gmail.com>
2022-09-18 18:11:19 +0200

    Build

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.static("dist"))
+
+const path = require("path")
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname,  "./dist/index.html")))
+
+let PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
